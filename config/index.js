cc11c06fc6bc202a1c409289cb1d1ae02e9f7193
chenkang950704 <112631037+chenkang950704@users.noreply.github.com>
2023-03-28 23:13:54 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,10 +10,10 @@ export const config = {
     APP_SECRET: "440dfb604f1a9e04cb45b3a3e2f7f9ae",
 
     // 模板消息id
-    TEMPLATE_ID: "1GMIXHaK_xxjHbqPQbU_SO74tWHHFCJOjbp0Geq-dq0",
+    TEMPLATE_ID: "qkYzXZgiuOlH61e1aNnbMd3sxegpPd36CBSrnouFT-4",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "1GMIXHaK_xxjHbqPQbU_SO74tWHHFCJOjbp0Geq-dq0",
+    CALLBACK_TEMPLATE_ID: "qkYzXZgiuOlH61e1aNnbMd3sxegpPd36CBSrnouFT-4",
 
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
